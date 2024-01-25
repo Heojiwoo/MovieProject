c417a5329ff1c634b51d7c17dab53341f46bfcba
Heojiwoo <anna001030@naver.com>
2024-01-25 15:41:46 +0900

    commit

--- src/main/java/com/lec/spring/controller/MovieController.java
+++ src/main/java/com/lec/spring/controller/MovieController.java
@@ -1,4 +1,15 @@
 package com.lec.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/movie")
 public class MovieController {
+
+//    @GetMapping("/home")
+//    public String test() {
+//        return "Hello, World";
+//    }
 }
